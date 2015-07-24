ea58d331115dd27af59bfc0e186af97b44f4721b
Hari kumar <hktangudu@gmail.com>
2015-07-24 15:44:46 +0530

    Modified

--- WallApp/app/models/user.rb
+++ WallApp/app/models/user.rb
@@ -2,6 +2,7 @@ class User
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
   include ActiveModel::SecurePassword
+  include Mongoid::Paperclip
 
   field :Name, type: String
   field :Age, type: Integer
@@ -12,4 +13,10 @@ class User
   field :password_digest, type: String
 
   has_secure_password
+
+  has_mongoid_attached_file :image, validate_media_type: false   
+  validates_attachment :image, :presence => true
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+  do_not_validate_attachment_file_type :image
 end
